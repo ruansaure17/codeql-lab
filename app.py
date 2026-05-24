e882788e7b66ce436f88bc4063b9144b879124c5
Ruan <saure.dev17@gmail.com>
2026-05-24 11:44:15 -0300

    Teste de aplicação vulnerável

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask, request
+import os
+
+app = Flask(__name__)
+
+@app.route('/ping')
+def ping():
+    host = request.args.get('host')
+
+    os.system(f"ping -c 1 {host}")
+
+    return "Ping executado"
+
+if __name__ == '__main__':
+    app.run(debug=True)
